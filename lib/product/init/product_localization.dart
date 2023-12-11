96aa991607d205f07934cec9516694b518fcc984
Yiğit TİLKİ <bestnemesis1234@gmail.com>
2023-12-12 00:48:21 +0300

    localization start

---
+++ lib/product/init/product_localization.dart
@@ -0,0 +1,9 @@
+import 'package:easy_localization/easy_localization.dart';
+
+final class ProductLocalization extends EasyLocalization {
+  ProductLocalization({required super.child, super.key})
+      : super(
+          supportedLocales: const [],
+          path: "asset/translations",
+        );
+}
