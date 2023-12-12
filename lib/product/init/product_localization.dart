9e217af7c599f09b417038260eeac373dd7ef79c
Yiğit TİLKİ <bestnemesis1234@gmail.com>
2023-12-12 19:07:28 +0300

    localization completed

--- lib/product/init/product_localization.dart
+++ lib/product/init/product_localization.dart
@@ -1,9 +1,26 @@
+import 'package:architecture_template/product/utility/constants/enums/locales.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 
+@immutable
 final class ProductLocalization extends EasyLocalization {
+  /// ProductLocalization need to [child] for a wrap locale items
   ProductLocalization({required super.child, super.key})
       : super(
-          supportedLocales: const [],
-          path: "asset/translations",
-        );
+            supportedLocales: _supportedItems,
+            path: _translationPath,
+            useOnlyLangCode: true);
+
+  static final List<Locale> _supportedItems = [
+    Locales.tr.locale,
+    Locales.en.locale,
+  ];
+  static const String _translationPath = 'assets/translations';
+
+  /// Change project language by using [Locales]
+  static Future<void> updateLanguage({
+    required BuildContext context,
+    required Locales value,
+  }) =>
+      context.setLocale(value.locale);
 }
