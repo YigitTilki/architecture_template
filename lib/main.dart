8452919ca48f570f486db74e52f84905052f8e89
Yiğit TİLKİ <bestnemesis1234@gmail.com>
2023-12-13 14:37:17 +0300

    app_init

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:architecture_template/feature/home/view/home_view.dart';
+import 'package:architecture_template/product/init/application_init.dart';
 import 'package:architecture_template/product/init/product_localization.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
-  runApp(ProductLocalization(child: const MyApp()));
+  await ApplicationInitialize().make();
+  runApp(
+    ProductLocalization(
+      child: const MyApp(),
+    ),
+  );
 }
 
 ///
--- lib/product/init/product_localization.dart
+++ lib/product/init/product_localization.dart
@@ -3,13 +3,16 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 @immutable
+
+///
 final class ProductLocalization extends EasyLocalization {
   /// ProductLocalization need to [child] for a wrap locale items
   ProductLocalization({required super.child, super.key})
       : super(
-            supportedLocales: _supportedItems,
-            path: _translationPath,
-            useOnlyLangCode: true);
+          supportedLocales: _supportedItems,
+          path: _translationPath,
+          useOnlyLangCode: true,
+        );
 
   static final List<Locale> _supportedItems = [
     Locales.tr.locale,
